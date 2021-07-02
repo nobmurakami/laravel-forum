8eb6c5f55f043a8ce6b59b41c90e642e9c5b4bfc
nobmurakami <mhpdm541@gmail.com>
2021-07-03 08:15:47 +0900

    スレッド名のカラム名を変更

--- backend/database/migrations/2021_07_03_075621_create_threads_table.php
+++ backend/database/migrations/2021_07_03_075621_create_threads_table.php
@@ -17,7 +17,7 @@ class CreateThreadsTable extends Migration
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->string('name', 255);
+            $table->string('title', 255);
             $table->timestamps();
             $table->softDeletes();
         });
