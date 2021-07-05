8d117f9e8f2a9f6743fb8693a8d6deab7086d2ad
nobmurakami <mhpdm541@gmail.com>
2021-07-05 21:46:54 +0900

    シーダーの更新

--- backend/database/seeds/UsersTableSeeder.php
+++ backend/database/seeds/UsersTableSeeder.php
@@ -19,16 +19,22 @@ class UsersTableSeeder extends Seeder
                 'name' => 'taro',
                 'email' => 'taro@test.com',
                 'password' => Hash::make('password'),
+                'created_at' => '2021-06-02 10:00:00',
+                'updated_at' => '2021-06-02 10:00:00',
             ],
             [
                 'name' => 'hanako',
                 'email' => 'hanako@test.com',
                 'password' => Hash::make('password'),
+                'created_at' => '2021-06-02 10:00:00',
+                'updated_at' => '2021-06-02 10:00:00',
             ],
             [
                 'name' => '寿限無寿限無五劫のすりきれ海砂利水魚の水行末雲来末風来末食う寝るところに住むところやぶら小路のぶら小',
                 'email' => 'jugemu@test.com',
                 'password' => Hash::make('password'),
+                'created_at' => '2021-06-02 10:00:00',
+                'updated_at' => '2021-06-02 10:00:00',
             ],
         ]);
 
