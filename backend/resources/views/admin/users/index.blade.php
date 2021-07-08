6a1cb6213a03d7ae4054bc1ec4a223e88f6261af
nobmurakami <mhpdm541@gmail.com>
2021-07-08 10:32:32 +0900

    DataTablesの日本語化

--- backend/resources/views/admin/users/index.blade.php
+++ backend/resources/views/admin/users/index.blade.php
@@ -45,7 +45,11 @@
 @section('js')
 <script>
     $(function () {
-        $("#users-table").DataTable();
+        $("#users-table").DataTable( {
+            language: {
+                url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Japanese.json'
+            }
+        });
     });
 </script>
 @stop
